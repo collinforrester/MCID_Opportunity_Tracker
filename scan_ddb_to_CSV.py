b8732e4cf15a4f5f15e16e1468c0ec15fcc61944
Wallace Printz <printw@amazon.com>
2019-03-03 09:55:52 -0600

    Fixed Python code

--- scan_ddb_to_CSV.py
+++ scan_ddb_to_CSV.py
@@ -1,29 +1,40 @@
-#!/usr/bin/env python
-
 import boto3
 from pprint import pprint
 import csv
+import json
+import random
 
+# Update the DynamoDB table information after it's created by Elastic Beanstalk
+ddb_table = 'awseb-e-7c68nzyeq7-stack-MCIDIdeasTable-PH0OQLXZC3RI'
+
+
+# Get access credentials for Dynamo
 ssmclient = boto3.client('ssm')
 ssmresponse = ssmclient.get_parameter(
     Name='DynamoDB_Reader_User_Creds',
     WithDecryption=True
 )
 
-ddb_access_key_id = ssmresponse['Parameter']['Value']
+ddb_creds = json.loads(ssmresponse['Parameter']['Value'])
+ddb_access_key = ddb_creds['Access key ID']
+ddb_secret_key = ddb_creds['Secret access key']
 
+
+# Scan the Dynamo table
 ddbclient = boto3.client(
     'dynamodb',
-    aws_access_key_id="<access key>",
-    aws_secret_access_key="<secret access key>",
+    aws_access_key_id=ddb_access_key,
+    aws_secret_access_key=ddb_secret_key,
     region_name='us-east-1'
     )
 
-response = client.scan(TableName='<Table Name>')
+ddbresponse = ddbclient.scan(TableName=ddb_table)
 
-if response['Count'] > 0:
 
-    table_header=list(response['Items'][0].keys())
+# Write output from table to CSV
+if ddbresponse['Count'] > 0:
+
+    table_header=list(ddbresponse['Items'][0].keys())
 
     # open a file for writing
     data = open('ddb_output.csv', 'w')
@@ -44,3 +55,11 @@ if response['Count'] > 0:
 
 else:
     print('Dynamo table is empty. No output written to disk.')
+
+
+
+# Select random winner
+winner = random.choice(range(ddbresponse['Count']))
+
+print('Winner is Idea #' + str(winner))
+print(ddbresponse['Items'][winner]['Name']['S'])
